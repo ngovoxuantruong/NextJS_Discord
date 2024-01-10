274202ff33b75d78d2f12cde7feac465b5aea855
Ngô Võ Xuân Trường <ngovoxuantruong8@gmail.com>
2024-01-10 20:12:56 +0700

    Initial model UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,26 @@
-const Setup = () => {
-  return <div>Create a server</div>;
+import { InitialModel } from "@/components/models/initial-model";
+import db from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModel></InitialModel>;
 };
 
-export default Setup;
+export default SetupPage;
